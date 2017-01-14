42e6cf66ae989ff25a36a41366eb5e63d9296c94
kaidzen <karpov.srg@gmail.com>
2017-01-14 13:38:09 +0200

    Initial with credentials

---
+++ src/com/brainsuper/simpleapp/SimpleApp.java
@@ -0,0 +1,16 @@
+package com.brainsuper.simpleapp;
+
+/**
+ * Created by Admin on 1/14/2017.
+ */
+public class SimpleApp {
+
+    public static final String HELLO_WORLD = "Hello World";
+
+    public static void main(String[] args) {
+
+        System.out.println(HELLO_WORLD);
+
+        System.out.println(HELLO_WORLD);
+    }
+}
