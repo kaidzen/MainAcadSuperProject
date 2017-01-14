7726b05b82a8745b4a96b94bd262678725cc27b1
Kaidzen <karpov.srg@gmail.com>
2017-01-14 22:24:17 +0200

    Add Lab_1-4-1

---
+++ src/org/kiadzen/lab1_4/Lab1_4_1.java
@@ -0,0 +1,26 @@
+package org.kiadzen.lab1_4;
+
+/**
+ * Created by Fortune on 14.01.2017.
+ */
+public class Lab1_4_1 {
+    public static void main(String[] args) {
+            byte    varByte = 93;
+            short   varShort = 1256;
+            int     varInt = 11220;
+            float   varFloat = 12.78F;
+            double  varDouble = 64.7658;
+            long    varLong=560_217_000L;
+            char    varChar='D';
+            boolean varBoolean=false;
+
+            System.out.println("byte varByte = " +varByte);
+            System.out.println("short varShort =" +varShort);
+            System.out.println("int varInt = " +varInt);
+            System.out.println("float varFloat = " +varFloat);
+            System.out.println("double varDouble = " +varDouble);
+            System.out.println("long varLong = " +varLong);
+            System.out.println("char varChar = " +varChar);
+            System.out.println("boolean varBoolean = " +varBoolean);
+    }
+}
