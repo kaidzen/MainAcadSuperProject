d7cdab37675e002bcc5ada63f13365b9ef588bf9
Kaidzen <karpov.srg@gmail.com>
2017-01-14 23:08:34 +0200

    Add Lab_1-4-3, edited

--- src/org/kiadzen/lab1_4/Lab1_4_3.java
+++ src/org/kiadzen/lab1_4/Lab1_4_3.java
@@ -16,11 +16,11 @@ public class Lab1_4_3 {
         boolean a4 = false;
         boolean b4 =false;
 
-        System.out.println("  A\t\t\t B\t\t\tA AND B\t\tA OR B\t\tA XOR B\t\t!A");
-        System.out.println(a1+"\t|\t"+b1+"\t|\t"+(a1&b1)+"\t|\t"+(a1|b1)+"\t|\t"+(a1^b1)+"\t|\t"+!a1);
-        System.out.println(a2+"\t|\t"+b2+"\t|\t"+(a2&b2)+"\t|\t"+(a2|b2)+"\t|\t"+(a2^b2)+"\t|\t"+!a2);
-        System.out.println(a3+"\t|\t"+b3+"\t|\t"+(a3&b3)+"\t|\t"+(a3|b3)+"\t|\t"+(a3^b3)+"\t|\t"+!a3);
-        System.out.println(a4+"\t|\t"+b4+"\t|\t"+(a4&b4)+"\t|\t"+(a4|b4)+"\t|\t"+(a4^b4)+"\t|\t"+!a4);
+        System.out.println("  A\t\t\t B\t\t\tA AND B\t\tA OR B\t\tA XOR B\t\t!A\t\t\t!B");
+        System.out.println(a1+"\t|\t"+b1+"\t|\t"+(a1&b1)+"\t|\t"+(a1|b1)+"\t|\t"+(a1^b1)+"\t|\t"+!a1+"\t|\t"+!b1);
+        System.out.println(a2+"\t|\t"+b2+"\t|\t"+(a2&b2)+"\t|\t"+(a2|b2)+"\t|\t"+(a2^b2)+"\t|\t"+!a2+"\t|\t"+!b2);
+        System.out.println(a3+"\t|\t"+b3+"\t|\t"+(a3&b3)+"\t|\t"+(a3|b3)+"\t|\t"+(a3^b3)+"\t|\t"+!a3+"\t|\t"+!b3);
+        System.out.println(a4+"\t|\t"+b4+"\t|\t"+(a4&b4)+"\t|\t"+(a4|b4)+"\t|\t"+(a4^b4)+"\t|\t"+!a4+"\t|\t"+!b4);
 
     }
 }
