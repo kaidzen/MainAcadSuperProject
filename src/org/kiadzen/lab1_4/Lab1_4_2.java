c8bc71dc23c04606104c4ebba7f91c92f6adacba
Kaidzen <karpov.srg@gmail.com>
2017-01-14 22:28:39 +0200

    Add Lab_1-4-2

---
+++ src/org/kiadzen/lab1_4/Lab1_4_2.java
@@ -0,0 +1,18 @@
+package org.kiadzen.lab1_4;
+
+/**
+ * Created by Fortune on 14.01.2017.
+ */
+public class Lab1_4_2 {
+    public static void main(String[] args) {
+
+        int a = 105;
+        int b = 32;
+
+        System.out.println("Operation a+b=" +(a+b));
+        System.out.println("Operation a-b=" +(a-b));
+        System.out.println("Operation a*b=" +a*b);
+        System.out.println("Operation a/b=" +a/b);
+        System.out.println("Operation a%b=" +a%b);
+    }
+}
